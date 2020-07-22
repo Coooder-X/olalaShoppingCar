280fc9a039474023cc5daa74a50ddcbeedd7460f
zhaoy0721 <zhaoyan1@goktech.cn>
2020-07-22 21:01:49 +0800

    管理后台页面提交

---
+++ src/main/java/com/goktech/olala/client/controller/backstage/MemberController.java
@@ -0,0 +1,13 @@
+package com.goktech.olala.client.controller.backstage;
+
+import com.goktech.olala.client.controller.basic.BasicController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/memberApi")
+public class MemberController extends BasicController {
+
+
+
+}
